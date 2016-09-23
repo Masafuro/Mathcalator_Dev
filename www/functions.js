f054b5aac1efd77db702e8e88512c5fc0fb000ff
Masafuro <apa226@gmail.com>
2016-09-23 21:10:59 +0900

    2016-09-23
    localStrageにRankを（S,SS,A~E）設定。それぞれのRankをDOMでHTMLに表示、書き換え。

--- www/functions.js
+++ www/functions.js
@@ -1,3 +1,11 @@
 function random(min,max){
   return Math.floor( Math.random() * (max + 1 - min) ) + min ;
 }
+
+function datatoint(n){
+    if (n == null){
+        n = 0;
+    }
+    n = parseInt(n);
+    return n;
+}
