63601f3f322b366e7ab0c3d4781b5f904f5a8660
Masafuro <apa226@gmail.com>
2016-10-01 09:18:04 +0900

    2016-10-01
    getCSV3.jsをfunctions.jsに統合

--- www/functions.js
+++ www/functions.js
@@ -1,3 +1,25 @@
+
+res = []; //getCSVとconvertCSVtoArrayで共通使用
+function getCSV(filename){
+    var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
+    req.open("get", "./data/"+filename , true); // アクセスするファイルを指定
+    req.send(null); // HTTPリクエストの発行
+
+    req.onload = function(){
+	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
+    }
+return res;
+}
+
+function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
+    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
+
+    for(var i=0;i<tmp.length;++i){
+        res[i] = tmp[i].split('\:');
+    }
+}
+
+
 function random(min,max){
   return Math.floor( Math.random() * (max + 1 - min) ) + min ;
 }
@@ -10,7 +32,7 @@ function datatoint(n){
     return n;
 }
 
-function load(){
+function generate(){
 
   var N = datatoint(result[0][0]);
   var data = random(1,N);
