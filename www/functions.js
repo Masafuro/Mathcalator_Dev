e436d737e3d6125016eb83c26909607472dd3911
Masafuro <apa226@gmail.com>
2016-10-03 09:56:43 +0900

    2016-10-03
    データ取得からの問題表示、正不判定、問題再表示、が完成。CSV読み込みでは、変数宣言を含むため、変数を利用する変数宣言（x=y+z）などは、配列の前の方に集め、後ろから読み込む方式に変更。大変心労したが、やればできる。

--- www/functions.js
+++ www/functions.js
@@ -1,22 +1,23 @@
 
-res = []; //getCSVとconvertCSVtoArrayで共通使用
+
 function getCSV(filename){
+
     var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
     req.open("get", "./data/"+filename , true); // アクセスするファイルを指定
     req.send(null); // HTTPリクエストの発行
 
     req.onload = function(){
-	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
+      var str = req.responseText;
+      var tmp = str.split("\n");
+      for(var i=0;i<tmp.length;++i){
+          result[i] = tmp[i].split('\:');
+      }
     }
-return res;
-}
+
+  }
 
 function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
-    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
-
-    for(var i=0;i<tmp.length;++i){
-        res[i] = tmp[i].split('\:');
-    }
+    // 改行を区切り文字として行を要素とした配列を生成
 }
 
 
@@ -25,7 +26,7 @@ function random(min,max){
 }
 
 function datatoint(n){
-    if (n == null){
+    if (n === null){
         n = 0;
     }
     n = parseInt(n);
@@ -37,22 +38,21 @@ function generate(){
   var N = datatoint(result[0][0]);
   var data = random(1,N);
 
-  var maxData = result[data][1];
-
-  var i;
-  for(i=5;i<=maxData;i++){
-  eval(result[data][i]);
-  }
-
   variable = [];
   variable = result[data][4].split(",");
-  equation = result[data][2]
-  ans = eval(result[data][3]);
+  equation = result[data][2];
 
+
+  var maxData = result[data][1];
+  var i;
+  for(i=maxData;i>=5;i--){
+  eval(result[data][i]);
+  }
 
   for(i=0;i<variable.length;i++){
       equation = equation.replace(new RegExp(variable[i],"g"),eval(variable[i]));
   }
 
+  ans = eval(result[data][3]);
 
 }
